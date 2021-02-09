c3ca9c9c66e7074d835dd0a7fe41d0cdd1b374a1
Alex Peterson <petersonale@umsystem.edu>
2021-02-09 12:31:29 -0700

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Alex Peterson on 2/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
